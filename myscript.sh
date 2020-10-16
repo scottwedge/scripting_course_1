e6d7ca15a459bc93f85cf09119e48620b78b600e
Scott Wedge <scott.wedge@gmail.com>
2020-10-16 17:39:39 -0400

    Simple IF and IF-ELSE conditional statements

--- myscript.sh
+++ myscript.sh
@@ -6,10 +6,25 @@
 # VARIABLEs
 # Upper case by convestion
 # Letters, numbers, underscores
-# NAME="Scott"
+NAME="Scott"
 # echo "My name is $NAME"
 # echo "My name is ${NAME}"
 
 # Read User Input
-read -p "Enter a number: " NUM
-echo "You entered value of $NUM."
+read -p "Enter a name: " VAL
+echo "You entered value of $VAL."
+
+# Simple IF statement
+if [ "$VAL" == "Scott" ]
+then
+   echo "Name matches"
+fi
+
+# Simple IF-ELSE statement
+if [ "$NAME" == "$VAL" ]
+then
+   echo "Name $VAL matches"
+else
+   echo "$NAME does not match $VAL"
+fi
+
