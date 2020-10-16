3a71d944c6bee49d0afc6f89f1b44d6135eccb5f
Scott Wedge <scott.wedge@gmail.com>
2020-10-16 19:15:27 -0400

    Simple '-gt' comparison

--- myscript.sh
+++ myscript.sh
@@ -6,13 +6,13 @@
 # VARIABLEs
 # Upper case by convestion
 # Letters, numbers, underscores
-NAME="Scott"
+# NAME="Scott"
 # echo "My name is $NAME"
 # echo "My name is ${NAME}"
 
 # Read User Input
-read -p "Enter a name: " VAL
-echo "You entered value of $VAL."
+# read -p "Enter a name: " VAL
+# echo "You entered value of $VAL."
 
 # Simple IF statement
 # if [ "$VAL" == "Scott" ]
@@ -29,13 +29,24 @@ echo "You entered value of $VAL."
 # fi
 
 # Simple ELSE-IF (elif) conditional statement
-if [ "$VAL" == "Brad" ]
-then
-    echo "You entered Brad"
-elif [ "$VAL" == "Dave" ]
-then
-    echo "You entered Dave"
-else
-    echo "You entered something other than Brad or Dave"
-fi
+# if [ "$VAL" == "Brad" ]
+# then
+#     echo "You entered Brad"
+# elif [ "$VAL" == "Dave" ]
+# then
+#     echo "You entered Dave"
+# else
+#     echo "You entered something other than Brad or Dave"
+# fi
 
+# Comparison
+# Use -eq  -ge  -gt -ge -lt -le within brackets to compare values
+read -p "Enter first value: " VAL1
+read -p "Enter second value: " VAL2
+
+if [ "$VAL1" -gt "$VAL2" ]
+then
+    echo "$VAL1 is greater than $VAL2"
+else
+    echo "$VAL1 is not greater than $VAL2"
+fi
