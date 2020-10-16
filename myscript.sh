f6f2866ad2430ee32c9fc6efbbc99100b416dbee
Scott Wedge <scott.wedge@gmail.com>
2020-10-16 15:25:16 -0400

    Simple echo  command, and

---
+++ myscript.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Echo command
+echo "Hello World"
+
+# VARIABLEs
+# Upper case by convestion
+# Letters, numbers, underscores
+NAME="Scott"
+echo "My name is $NAME"
+echo "My name is ${NAME}"
