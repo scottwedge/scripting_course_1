74062c3c7e470ac397eb9e65aa4ec7fc8e825f9a
Scott Wedge <scott.wedge@gmail.com>
2020-10-16 17:24:44 -0400

    Read input from user

--- myscript.sh
+++ myscript.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
 # Echo command
-echo "Hello World"
+# echo "Hello World"
 
 # VARIABLEs
 # Upper case by convestion
 # Letters, numbers, underscores
-NAME="Scott"
-echo "My name is $NAME"
-echo "My name is ${NAME}"
+# NAME="Scott"
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
+
+# Read User Input
+read -p "Enter a number: " NUM
+echo "You entered value of $NUM."
