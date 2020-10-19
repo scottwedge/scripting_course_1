da2c28cb19e98d773e24d122f73b5f23cd985f8e
Scott Wedge <scott.wedge@gmail.com>
2020-10-18 20:34:24 -0400

    Practice with simple math

--- myscript.sh
+++ myscript.sh
@@ -41,27 +41,36 @@
 
 # Comparison
 # Use -eq  -ge  -gt -ge -lt -le within brackets to compare values
-read -p "Enter first value: " VAL1
-read -p "Enter second value: " VAL2
+# read -p "Enter first value: " VAL1
+# read -p "Enter second value: " VAL2
 
-if [ "$VAL1" -eq "$VAL2" ]
-then
-    echo "$VAL1 equals $VAL2"
-elif [ "$VAL1" -gt "$VAL2" ]
-then
-    echo "$VAL1 is greater than $VAL2"
-elif [ "$VAL1" -lt "$VAL2" ]
-then 
-    echo "$VAL1 is less than $VAL2"
-fi
+# if [ "$VAL1" -eq "$VAL2" ]
+# then
+#     echo "$VAL1 equals $VAL2"
+# elif [ "$VAL1" -gt "$VAL2" ]
+# then
+#     echo "$VAL1 is greater than $VAL2"
+# elif [ "$VAL1" -lt "$VAL2" ]
+# then 
+#     echo "$VAL1 is less than $VAL2"
+# fi
 
-read -p "Enter third value: " VAL3
-read -p "Enter fourth value: " VAL4
+# read -p "Enter third value: " VAL3
+# read -p "Enter fourth value: " VAL4
 
-if [ "$VAL3" -le "$VAL4" ]
-then
-    echo "$VAL3 is less than or equal to $VAL4"
-elif [ "$VAL3" -ge "$VAL4" ]
-then
-    echo "$VAL3 is greater than or equal to $VAL4"
-fi
+# if [ "$VAL3" -le "$VAL4" ]
+# then
+#     echo "$VAL3 is less than or equal to $VAL4"
+# elif [ "$VAL3" -ge "$VAL4" ]
+# then
+#     echo "$VAL3 is greater than or equal to $VAL4"
+# fi
+
+# Simple math inside double brackets "(( ))"
+read -p "Enter a value that will be doubled:" VAL5
+echo "You entered: $VAL5"
+echo "Double $VAL5 this value is: $((2*$VAL5))"
+
+read -p "Enter first of two numbers to multiply: " VAL6
+read -p "Enter second of two numbers to multiply: " VAL7
+echo "Product of $VAL6 and $VAL7 is $(($VAL6 * $VAL7))"
