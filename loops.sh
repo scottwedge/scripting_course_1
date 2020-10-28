db343f7a341b54f8b2996e671f276cc45ebadc34
swedge <scott.wedge@gmail.com>
2020-10-28 15:33:39 -0400

    Until loop

--- loops.sh
+++ loops.sh
@@ -8,10 +8,22 @@
 #   statements
 # done
 
-j=1
-while [ $j -le 10 ]
+# j=1
+# while [ $j -le 10 ]
+# do
+#   echo $j
+#   # j=$(( $j + 1 )) 
+#   (( j++ ))
+# done
+ 
+# UNTIL
+# until [ <some test> }
+# do
+#     statements
+# done
+
+until [ $j -gt 20 ]
 do
   echo $j
-  # j=$(( $j + 1 )) 
   (( j++ ))
 done
