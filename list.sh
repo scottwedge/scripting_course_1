e86e58eea870699803c350c315c84afd90928d9a
swedge <scott.wedge@gmail.com>
2020-11-01 17:27:34 -0500

    Move logic into 'if' part of loop

--- list.sh
+++ list.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Check if optional parameter exists else default to current directory
+
+if [ $# -ge 1 ]
+then
+  echo "at least one parameter exists"
+  for value in $1/*
+  do
+    echo $value
+  done
+else
+  echo " No optional parameters so defaulting to current directory"
+fi
+
 for value in $1/*
 do
   echo $value
